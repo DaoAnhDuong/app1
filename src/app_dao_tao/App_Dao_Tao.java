4cf59d748e355610ca51b93922f31a2baa08ce9a
ADMIN <ADMIN@M>
2021-12-10 19:49:05 +0700

    Init Project

---
+++ src/app_dao_tao/App_Dao_Tao.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app_dao_tao;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class App_Dao_Tao {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new LoginForm().setVisible(true);
+    }
+    
+}
